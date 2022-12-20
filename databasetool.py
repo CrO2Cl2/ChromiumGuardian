ff704075d21f65c368c21353ac9a36851396285e
CrO2Cl2 <111274010+CrO2Cl2@users.noreply.github.com>
2022-12-20 15:29:38 +0100

    fixed errors. the algorythm still is shit. it is not usable in this state

---
+++ databasetool.py
@@ -0,0 +1,23 @@
+import csv
+import os
+
+# Read in the label
+label = input("Enter the label (spam or not spam): ")
+
+# Open a CSV file for writing in append mode
+with open('spam_data.csv', 'a', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    
+    # Check if the file is empty
+    if os.stat('spam_data.csv').st_size == 0:
+        # If the file is empty, write the header row
+        writer.writerow(['text', 'label'])
+    
+    # Loop indefinitely
+    while True:
+        # Read in the message
+        message = input("Enter the message: ")
+        
+        # Write the message and label as a row
+        writer.writerow([message, label])
+        
---
+++ spamdec.py
@@ -0,0 +1,37 @@
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+
+# Read in the data
+df = pd.read_csv('spam_data.csv')
+
+# Create a CountVectorizer object
+vectorizer = CountVectorizer()
+
+# Transform the texts into a numerical representation
+X = vectorizer.fit_transform(df['text'])
+
+# Extract the labels
+y = df['label']
+
+# Split the data into train and test sets
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Create a MultinomialNB object
+clf = MultinomialNB()
+
+# Fit the model to the training data
+clf.fit(X_train, y_train)
+
+# Define the message to classify
+message = "input the message to classify here"               # INPUT THE MESSAGE TO CLASSIFY HERE
+
+# Transform the message into a numerical representation
+X_message = vectorizer.transform([message])
+    
+# Make a prediction on the message
+prediction = clf.predict(X_message)[0]
+
+# Print the prediction
+print(f"Prediction: {prediction}")
