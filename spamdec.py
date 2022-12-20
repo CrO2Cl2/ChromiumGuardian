267b4af14b9f8bd4c35bbda0fc44fc3ecddd9f57
CrO2Cl2 <111274010+CrO2Cl2@users.noreply.github.com>
2022-12-20 17:32:50 +0100

    added messages

--- spamdec.py
+++ spamdec.py
@@ -1,9 +1,12 @@
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
 
 # Read in the data
-df = pd.read_csv('spam_data.csv', error_bad_lines=False)
+df = pd.read_csv('spam_data.csv', error_bad_lines=False, encoding='windows-1252')
 
 # Create a CountVectorizer object
 vectorizer = CountVectorizer()
@@ -12,7 +15,7 @@ vectorizer = CountVectorizer()
 X = vectorizer.fit_transform(df['text'])
 
 # Extract the labels
-y = df['label']
+y = df['label'].values
 
 # Split the data into train and test sets
 from sklearn.model_selection import train_test_split
@@ -25,21 +28,14 @@ clf = MultinomialNB()
 clf.fit(X_train, y_train)
 
 # Define the message to classify
-message = "guhvdshjgkdsf"                                                       # INPUT THE MESSAGE TO CLASSIFY HERE
+messages = ["ewr"]                     # INPUT THE MESSAGE TO CLASSIFY HERE
 
-# Transform the message into a numerical representation
-X_message = vectorizer.transform([message])
+for message in messages:
+    # Transform the message into a numerical representation
+    X_message = vectorizer.transform([message])
+        
+    # Make a prediction on the message
+    prediction = clf.predict(X_message)[0]
     
-# Make a prediction on the message
-prediction = clf.predict(X_message)[0]
-
-# Print the prediction
-print(f"Prediction: {prediction}")
-
-X_message = vectorizer.transform([message])
-    
-# Make a prediction on the message
-prediction = clf.predict(X_message)[0]
-
-# Print the prediction
-print(f"Prediction: {prediction}")
+    # Print the prediction
+    print(f"Prediction: {prediction}")
